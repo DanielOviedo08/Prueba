142c658ba2dc0723082f4af2f378383ae154c9a6
DanielOviedo08 <20203tn140@utez.edu.mx>
2023-02-21 23:12:13 -0600

    Primer Commit

---
+++ script.js
@@ -0,0 +1,12 @@
+//Java Script
+
+function cambiarMensaje(){
+    document.getElementById("enviado").innerText="Enviado";
+}
+
+setTimeout(cambiarMensaje,2300)
+
+
+setTimeout(function(){
+    alert("Te Quiero Di")
+},2400)
